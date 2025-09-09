bbe517a1a7a36810ac65a5fc3ec6899ea5eb091a
AslanSoft <beggyplayy@gmail.com>
2025-09-09 21:13:28 +0500

    train

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ for (i = 0; i > 10; i++) {
 
 // prettier settings added
 
+let myBtn = document.querySelector("myBtn");
+myBtn.addEventListener("click", function(){
+    myBtn.computedStyleMap.display === "none"
+})
