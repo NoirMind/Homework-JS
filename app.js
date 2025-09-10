56c39fa6d60ced177490020960c0d9b9f61d24ea
AslanSoft <beggyplayy@gmail.com>
2025-09-10 22:08:36 +0500

    cash train

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-let a = 'Salom'
+// let a = 'Salom'
 
-for (i = 0; i > 10; i++) {
-    console.log(i)
-}
+// for (i = 0; i > 10; i++) {
+//     console.log(i)
+// }
 
-// prettier settings added
+// // prettier settings added
 
-let myBtn = document.querySelector("myBtn");
-myBtn.addEventListener("click", function(){
-    myBtn.computedStyleMap.display === "none"
-})
+// let myBtn = document.querySelector("myBtn");
+// myBtn.addEventListener("click", function(){
+//     myBtn.computedStyleMap.display === "none"
+// })
+
+// // train
+
+let cash = prompt("Valyutani kiriting");
+let usd = 12450
+let uzs = usd * cash;
+
+alert (cash + " CASH " + uzs + " UZS ")
+
