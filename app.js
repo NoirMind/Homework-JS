8a36dfd046ac8533257be8b7a2733ecda946acda
AslanSoft <beggyplayy@gmail.com>
2025-09-13 19:39:40 +0500

    Js train

--- app.js
+++ app.js
@@ -13,9 +13,32 @@
 
 // // train
 
-let cash = prompt("Valyutani kiriting");
-let usd = 12450
-let uzs = usd * cash;
+// let cash = prompt("Valyutani kiriting");
+// let usd = 12450
+// let uzs = usd * cash;
 
-alert (cash + " CASH " + uzs + " UZS ")
+// alert (cash + " CASH " + uzs + " UZS ")
 
+engkattasi (2,55,1)
+
+function engkattasi(a,b,c){
+    let maxval = []
+    let minimum = []
+    let medium = []
+    let max = a 
+    if(b<max && c<max) {
+        console.log(max);
+    }else if(max < b && c < b){
+        console.log(b);
+    }
+    else{
+        console.log(c);
+    }
+    return [...minimum, ...maxval, ...medium]
+}
+
+console.log([] + {});
+console.log({} + []);
+
+
+console.log(typeof null);
